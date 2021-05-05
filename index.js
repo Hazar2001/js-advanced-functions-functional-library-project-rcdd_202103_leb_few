f54ab877e349eea4533b40cdf40bd9475545a218
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-05-05 09:07:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fi = (function() {
     },
 
     each: function() {
-
+      fi.each([1, 2, 3,4], alert);
+      fi.each({one: 1, two: 2, three: 3, four: 4}, alert);
     },
 
     map: function() {
